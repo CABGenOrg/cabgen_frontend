7d9179b19fae0dedcfdd7c75002850507c55a27e
Nicolas da Matta <nicolasaraujo47@gmail.com>
2024-02-29 09:48:30 -0300

    Feat: Created Section component.

---
+++ src/components/General/Section.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface SectionParams {
+  id: string;
+  children: React.ReactNode;
+  gray?: boolean;
+}
+
+const Section = ({ id, gray, children }: SectionParams) => {
+  return (
+    <section
+      id={id}
+      className={`container[max-width: 100%] w-screen h-screen flex items-center justify-center ${
+        gray ? "bg-gray-100" : "bg-white"
+      }`}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default Section;
