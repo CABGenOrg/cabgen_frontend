2e1ae64e846ae2bdc139c51cf7a99d6bc0fade21
Nicolas da Matta <nicolasaraujo47@gmail.com>
2024-03-04 19:05:55 -0300

    Feat: Created NetworkPurposes component.

--- src/app/network/page.tsx
+++ src/app/network/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NetworkDescription from "@/components/Network/NetworkDescription";
+import NetworkPurposes from "@/components/Network/NetworkPurposes";
 
 const Network = () => {
   return (
     <>
       <NetworkDescription />
+      <NetworkPurposes />
     </>
   );
 };
